a2c3334cb29f6b012150af5c20d3b732a4f59d52
Suzanne <suzanne.aitchison@gmail.com>
2017-01-12 14:49:21 +0000

    update numerals as shorted method

--- numerals9_5.rb
+++ numerals9_5.rb
@@ -1,37 +1,13 @@
-#Acquire input integer from user
-puts "Please enter the number to be converted to numberals (between 1 and 3000)"
-n = gets.chomp
-i = n.to_i
-#Determine number of M numerals
-m_numeral = i/1000
-m_numeral = m_numeral.floor
-i = i - (m_numeral*1000)
-#Determine number of D numerals
-d_numeral = i/500
-d_numeral = d_numeral.floor
-i = i - (d_numeral*500)
-#Determine number of C numerals
-c_numeral = i/100
-c_numeral = c_numeral.floor
-i = i - (c_numeral*100)
-#Determine number of L numerals
-l_numeral = i/50
-l_numeral = l_numeral.floor
-i = i - (l_numeral*50)
-#Determine number of X numerals
-x_numeral = i/10
-x_numeral = x_numeral.floor
-i = i - (x_numeral*10)
-#Determine number of V numerals
-v_numeral = i/5
-v_numeral = v_numeral.floor
-i = i - (v_numeral*5)
-#Output
-puts "The old style Roman numerals for the number #{n} are:"
-m_numeral.times {print "M"}
-d_numeral.times {print "D"}
-c_numeral.times {print "C"}
-l_numeral.times {print "L"}
-x_numeral.times {print "X"}
-v_numeral.times {print "V"}
-i.times {print "I"}
+
+def old_numerals(num)
+numerals = []
+numerals << "M"*(num/1000)
+numerals << "D"*(num%1000 /500)
+numerals << "C"*(num%500 / 100)
+numerals << "L"*(num%100 /50)
+numerals << "X"*(num%50 /10)
+numerals << "V"*(num%10 /5)
+numerals << "I"*(num%5)
+end
+
+print old_numerals(2315)
